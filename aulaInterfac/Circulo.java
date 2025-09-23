9d33b9f76ade1601979ad4bcf08eed1bd2a1a5d9
Iwerson <109252776+owleyes62@users.noreply.github.com>
2025-09-23 21:46:03 +0000

    atividade da sala

---
+++ aulaInterfac/Circulo.java
@@ -0,0 +1,17 @@
+public class Circulo implements FormaGeometrica {
+    private double raio;
+
+    public Circulo(double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * raio * raio;
+    }
+
+    @Override
+    public double comprimento() {
+        return 2 * Math.PI * raio;
+    }
+}
